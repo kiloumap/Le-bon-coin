80888c2dfb33dfec22ab9a9d3802ae89c712a1fa
unknown <laurent.martinez@ynov.com>
2018-04-19 17:34:04 +0200

    init

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ var app            = express();
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 const config       = require('config'); // load db location to split test and dev database
+const mongoose      = require('mongoose');
 
 // configuration ===========================================
 
 // config files
-var db = require('./config/data');
+var db = require('./config/db');
 
 // set our port
 var port          = 8080;
